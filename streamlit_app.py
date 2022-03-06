b9c007a7dc8574c897862d48765d3eff66d21c88
AKSHAT SRIVASTAVA <70881919+technickel03@users.noreply.github.com>
2022-03-06 10:28:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,30 +4,29 @@ import pandas as pd
 import numpy as np
 import datetime
 
-def viz(ticker_sym : str,date):
-    ticker = yf.download(ticker_sym,start=date,interval="1d")
+def viz(ticker_sym : str,date,interval):
+    ticker = yf.download(ticker_sym,start=date,interval=interval)
     df = ticker.copy()
     df = df[['Close']]
-    print(df)
-    #df.reset_index(level=0, inplace=True)
-    #df.columns=['ds','y']
     st.line_chart(df)
-    #plt.plot(df.ds, df.y)
-    #plt.show()
-    #plt.figure(figsize=(10,10))
     rolling_mean = df.Close.rolling(window=200).mean()
     rolling_mean2 = df.Close.rolling(window=50).mean()
     df["200 DMA"] = rolling_mean
     df["50 DMA"] = rolling_mean2
     st.line_chart(df)
-df = pd.read_csv("nifty500.csv")
-ticker  = st.selectbox("select company name",df["Company Name"])
-ticker_sym = df.loc[ticker == df["Company Name"],"Symbol"]
+choice = st.selectbox("Select Stocks/Crypto",("stocks","crypto"))
+if choice == "stocks":
+    df = pd.read_csv("nifty500.csv")
+    ticker  = st.selectbox("select company name",df["Company Name"])
+    ticker_sym = df.loc[ticker == df["Company Name"],"Symbol"][0]+".ns"
+else:
+    ticker_sym = st.text_input("Enter Crypto Ticker",placeholder = "Example : btc-inr , Eth-inr")
+st.markdown(str(ticker_sym))
 date_str = st.date_input("select date")
 formatted_date = datetime.datetime.strptime(str(date_str), "%Y-%m-%d").strftime("%Y-%m-%d")
-#interval_str = st.text_input("Valid intervals",placeholder="1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo")
+interval_str = st.selectbox("Valid intervals",("1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo").split(","))
 form = st.form(key='my_form')
 submit_button = form.form_submit_button(label='Submit')
 if submit_button:
     st.markdown(formatted_date)
-    viz(ticker_sym,formatted_date)
+    viz(ticker_sym,formatted_date,interval_str)
